f7ec2cb24352e8a113cdca9fd7b009427ad51725
Theodor Manolescu <manolescu.theodor@yahoo.com>
2015-08-25 21:44:36 +0300

    initial commit

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Class DefaultController
+ *
+ * @package AppBundle\Controller
+ */
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction()
+    {
+        return $this->render('default/index.html.twig');
+    }
+}
