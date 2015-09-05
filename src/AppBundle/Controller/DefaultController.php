0b89281d15da63ecc6b28caf1d778572cd92a3c7
Theodor Manolescu <theodor.manolescu@emag.ro>
2015-09-05 09:27:28 +0300

    updated bundle structure

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,19 +4,10 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-/**
- * Class DefaultController
- *
- * @package AppBundle\Controller
- */
 class DefaultController extends Controller
 {
-    /**
-     *
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function indexAction()
+    public function indexAction($name)
     {
-        return $this->render('default/index.html.twig');
+        return $this->render('AppBundle:Default:index.html.twig', array('name' => $name));
     }
 }
--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -4,21 +4,14 @@ namespace AppBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-/**
- * Class DefaultControllerTest
- *
- * @package AppBundle\Tests\Controller
- */
 class DefaultControllerTest extends WebTestCase
 {
-
     public function testIndex()
     {
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/');
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertTrue($crawler->filter('html:contains("Homepage")')->count() > 0);
+        $this->assertTrue($crawler->filter('html:contains("Hello")')->count() > 0);
     }
 }
